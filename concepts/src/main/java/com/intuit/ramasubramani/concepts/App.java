6c4d7a16a46f304411c64c71107bede2392e8fc3
rn5 <ramasubramani_n@intuit.com>
2019-11-03 13:16:19 +0530

    Setting up Spring Boot Project...

---
+++ concepts/src/main/java/com/intuit/ramasubramani/concepts/App.java
@@ -0,0 +1,13 @@
+package com.intuit.ramasubramani.concepts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
